46e1d71667d6e0a697ba4c79426fe4a5cca7c74e
bredom <domm747@gmil.com>
2022-10-13 16:55:39 +0200

    Changing server address

--- src/pages/Todos.js
+++ src/pages/Todos.js
@@ -6,7 +6,7 @@ import TodosList from '../components/TodosList';
 const Todos = () => {
   const [todos, setTodos] = useState([]);
 
-  const url = 'https://node-api-example-domm.herokuapp.com';
+  const url = 'https://node-api-example-bredom.onrender.com';
 
   useEffect(() => {
     async function getTodos() {
